ece90d7ac98c4d9795d7094d0a157b241ab01228
Aram Petrosyan <kane_02@mail.ru>
2023-04-16 22:35:37 +0600

    main: update main file with correct endpoints order example

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+# Because path operations are evaluated in order,
+# you need to make sure that the path for /users/me
+# is declared before the one for /users/{user_id}:
+
+# Otherwise, the path for /users/{user_id}
+# would match also for /users/me, "thinking"
+# that it's receiving a parameter user_id with a value of "me".
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
